b9794480a9f1e45c21317b94843182606806763a
Daeseong Yu <dyu8422@conestogac.on.ca>
2025-03-01 16:24:08 -0500

    - added user register and login

--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const policeNewsRoutes = require('./routes/policeNews');
 const arcgisRoutes = require('./routes/arcgis');
 const emergencyRoutes = require('./routes/emergency');
+const userRoutes = require('./routes/user');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use(cors());
 app.use('/api/police-news', policeNewsRoutes);
 app.use('/api/arcgis', arcgisRoutes);
 app.use('/api/emergency', emergencyRoutes);
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
